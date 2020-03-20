e32d35e11f64797923a8380238f598a633a524e5
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-03-20 10:41:57 +0200

    index-server-package-changes

---
+++ server.js
@@ -0,0 +1,18 @@
+//Import libraries
+const express = require("express");
+
+//Import routes
+// const ProjectRouter =require("./projects/project-router");
+// const ResourceRouter=require("./resources/resource-router");
+
+//Activate imports
+const server = express();
+
+//Inject import functionality
+server.use(express.json());
+
+//Declare server use routing
+// server.use("/api/projects", ProjectRouter);
+// server.use("/api/resource",ResourceRouter);
+
+module.exports = server;
